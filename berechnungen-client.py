dbe518f654dad7cd80b1225b3321ce74536673a0
Manuel Bärenz <manuel@enigmage.de>
2011-08-20 21:56:20 +0200

    Nicht das Rad neu erfinden: Shelve ist besser

--- berechnungen-client.py
+++ berechnungen-client.py
@@ -1,22 +1,9 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-import cPickle
-try:
-	with open("lokale_berechnungen.pickle") as lokale_berechnungen_datei:
-		lokale_berechnungen = cPickle.load(lokale_berechnungen_datei)
-except IOError as IOError_instanz:
-	if IOError_instanz.errno == 2: # errno 2 bedeutet dass die Datei nicht vorhanden ist. Dann wird sie neu erstellt.
-		print "lokale_berechnungen.pickle existiert nicht, wird neu erstellt werden"
-		lokale_berechnungen = {}
-	else:
-		handlung = ""
-		while handlung not in ("e", "n"):
-			raw_input("Warnung: lokale_berechnungen.pickle war unlesbar. Durch leere Datei ersetzen (e) oder nichts tun (n)?")
-		if handlung == "e":
-			lokale_berechnungen = {}
-		if handlung == "n":
-			exit("Nichts wird getan")
+import shelve
+
+lokale_berechnungen = shelve.open("lokale_berechnungen.shelve", writeback=True)
 
 import xml.dom.minidom
 anfrage_dokument = xml.dom.minidom.Document()
